2895db4c5c97a382a799bc38b1bc1fb209334a6d
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-12-27 10:20:51 +0530

    Design: Initial Design Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+export default function App() {
+  return <div className="hero h-screen w-screen">
+    <div className="flex justify-end p-10"><img src={process.env.PUBLIC_URL + '/zevi.svg'}/></div>
+    <div className="flex justify-center m-auto"> <input type="text" placeholder="Search Something Guud!!"/></div>
+  </div>
 }
-
-export default App;
